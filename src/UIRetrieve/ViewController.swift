841ce186073b59256a07a19dcdcbcb436bb2edba
Delmwin Baeka <dbaekajnr@sjtu.edu.cn>
2020-06-02 00:40:00 -0400

    update

---
+++ src/UIRetrieve/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  UIRetrieve
+//
+//  Created by Delmwin Baeka on 04/12/2019.
+//  Copyright © 2019 Delmwin Baeka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var tempImageView: UIImageView!
+    
+    // MARK: - Actions
+    
+    @IBAction func resetPressed(_ sender: Any) {
+    }
+    
+    @IBAction func sharePressed(_ sender: Any) {
+    }
+    
+    @IBAction func pencilPressed(_ sender: UIButton) {
+    }
+
+
+}
+
