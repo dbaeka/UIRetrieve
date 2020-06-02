b5d8c2bdfb8b624bbe15fc6c6a97ed5f5700bfcc
Delmwin Baeka <dbaekajnr@sjtu.edu.cn>
2020-06-02 00:39:06 -0400

    init

---
+++ src/UIRetrieve/Pencil.swift
@@ -0,0 +1,53 @@
+//
+//  Pencil.swift
+//  UIRetrieve
+//
+//  Created by Delmwin Baeka on 07/01/2020.
+//  Copyright © 2020 Delmwin Baeka. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum Pencil {
+  case black
+  case grey
+  case red
+  case darkblue
+  case lightBlue
+  case darkGreen
+  case lightGreen
+  case brown
+  case orange
+  case yellow
+  case eraser
+  
+  init?(tag: Int) {
+    switch tag {
+    case 1:
+      self = .black
+    case 2:
+      self = .grey
+    case 3:
+      self = .red
+    case 4:
+      self = .darkblue
+    case 5:
+      self = .lightBlue
+    case 6:
+      self = .darkGreen
+    case 7:
+      self = .lightGreen
+    case 8:
+      self = .brown
+    case 9:
+      self = .orange
+    case 10:
+      self = .yellow
+    case 11:
+      self = .eraser
+    default:
+      return nil
+    }
+  }
+}
---
+++ src/UIRetrieve/SettingsViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SettingsViewController.swift
+//  UIRetrieve
+//
+//  Created by Delmwin Baeka on 07/01/2020.
+//  Copyright © 2020 Delmwin Baeka. All rights reserved.
+//
+
+import UIKit
+
+
+class SettingsViewController: UIViewController {
+    
+    @IBOutlet weak var sliderBrush: UISlider!
+    @IBOutlet weak var sliderOpacity: UISlider!
+    @IBOutlet weak var previewImageView: UIImageView!
+    
+    @IBOutlet weak var labelBrush: UILabel!
+    @IBOutlet weak var labelOpacity: UILabel!
+    
+    @IBOutlet weak var sliderRed: UISlider!
+    @IBOutlet weak var sliderGreen: UISlider!
+    @IBOutlet weak var sliderBlue: UISlider!
+    
+    @IBOutlet weak var labelRed: UILabel!
+    @IBOutlet weak var labelGreen: UILabel!
+    @IBOutlet weak var labelBlue: UILabel!
+    
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      
+      // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func closePressed(_ sender: Any) {
+    }
+    
+    @IBAction func brushChanged(_ sender: UISlider) {
+    }
+    
+    @IBAction func opacityChanged(_ sender: UISlider) {
+    }
+    
+    @IBAction func colorChanged(_ sender: UISlider) {
+    }
+}
